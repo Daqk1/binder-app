78896b8afe4b6d48b14b414eaa9d40000f3ac23c
alvintran <tralvin3@gmail.com>
2025-07-03 13:06:39 -0700

    css

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -5,19 +5,20 @@ class Card extends Component {
 
     return (
           <div className="card">
-              <h3>{card.cardName}</h3>
               <img
                 src={card.cardPicture}
                 alt={card.cardName}
                 className = "image-container"
               />
-              <p>Price: ${card.cardPrice}</p>
               <div className="b">
                 <button className="minusButton" onClick={() => subtractCard(card)}>-</button>
-                <p className = "count">{card.cardNumberOfCards}</p>
                 <button className="addButton" onClick={() => addCard(card)}>+</button>
               </div>
               <a href={card.cardUrl} target="_blank" rel="noopener noreferrer">View Card</a>
+              <div className="card-badges-bottom">
+                <span className="count-badge">{card.cardNumberOfCards}</span>
+                <span className="price-badge">${card.cardPrice}</span>
+              </div>
             </div>
     );
   }
